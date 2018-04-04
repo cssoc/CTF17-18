1f4854bc7c5dd7f567bd670833365c1afa4e6667
3553x <3553x@tuta.io>
2018-04-04 02:09:41 +0100

    implemented lack of authentication, closes #23

---
+++ web/ajax/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE messages(
+recp TEXT NOT NULL,
+sndr TEXT NOT NULL,
+msg TEXT NOT NULL);
+
+INSERT INTO messages VALUES('admin', '???', 'CSSOC{Ju57_W4LK_in_4nd_74K3_I7_8R0}');
+
---
+++ web/ajax/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -dp 80:80 -m1g --name ajax_live ajax
