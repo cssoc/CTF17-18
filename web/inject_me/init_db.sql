342ab62d5f6c0313300c7e5ffb62535e14d44d40
3553x <3553x@tuta.io>
2018-04-04 20:00:43 +0100

    implemented client side validation, closes #21

---
+++ web/inject_me/init_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+	name TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL
+	);
+
+INSERT INTO users VALUES('admin','Y1iGk6sTaQozQQogdyaSdZFFvtoFR8a');
---
+++ web/inject_me/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -dp 80:80 -m1g --name inject_me_live inject_me
